cdae30998349d131244ebd671bdd255fc7cbf5a1
leonid <kotovpetia@gmail.com>
2022-11-02 14:24:43 +0300

    GroupCommandHandlerTest added

--- src/main/java/org/foxminded/springcourse/consoleapp/model/Group.java
+++ src/main/java/org/foxminded/springcourse/consoleapp/model/Group.java
@@ -23,6 +23,10 @@ public class Group {
     public Group() {
     }
 
+    public Group(String name) {
+        this.name = name;
+    }
+
     public Group(int id, String name) {
         this.id = id;
         this.name = name;
