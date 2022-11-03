1e8aa7cfee28a3902c93fdb0fb8c0bc502fca1f6
leonid <kotovpetia@gmail.com>
2022-11-03 10:24:09 +0300

    sql script text -> varchar

--- db-init/init.sql
+++ db-init/init.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS students_courses CASCADE;
 CREATE TABLE groups
 (
     group_id   SERIAL PRIMARY KEY,
-    group_name TEXT,
+    group_name VARCHAR(20),
 
     CONSTRAINT uq_groups_group_name
         UNIQUE (group_name)
@@ -16,8 +16,8 @@ CREATE TABLE students
 (
     student_id SERIAL PRIMARY KEY,
     group_id   INTEGER,
-    first_name TEXT,
-    last_name  TEXT,
+    first_name VARCHAR(50),
+    last_name  VARCHAR(50),
 
     CONSTRAINT fk_students_groups
         FOREIGN KEY (group_id)
@@ -27,8 +27,8 @@ CREATE TABLE students
 CREATE TABLE courses
 (
     course_id          SERIAL PRIMARY KEY,
-    course_name        TEXT,
-    course_description TEXT,
+    course_name        VARCHAR(30),
+    course_description VARCHAR(50),
 
     CONSTRAINT uq_courses_course_name
         UNIQUE (course_name)
