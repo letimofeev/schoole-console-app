b95a5fdd3da59f75adf570dc91baac2fe57a4586
leonid <kotovpetia@gmail.com>
2022-11-04 23:15:29 +0300

    GroupDaoJdbcTest.java added

---
+++ src/test/java/org/foxminded/springcourse/consoleapp/dao/GroupDaoJdbcTest.java
@@ -0,0 +1,71 @@
+package org.foxminded.springcourse.consoleapp.dao;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.shell.jline.InteractiveShellApplicationRunner;
+import org.springframework.shell.jline.ScriptShellApplicationRunner;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Transactional
+@Testcontainers
+@SpringBootTest(properties = {
+        InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false",
+        ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"
+})
+class GroupDaoJdbcTest {
+
+    @Autowired
+    private GroupDao groupDao;
+
+    @Container
+    private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:13.3")
+            .withDatabaseName("school-test")
+            .withInitScript("groups_test_tables.sql");
+
+    @BeforeAll
+    static void setUp() {
+        container.start();
+    }
+
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+        registry.add("spring.datasource.driver-class-name", container::getDriverClassName);
+    }
+
+    @AfterAll
+    static void tearDown() {
+        container.stop();
+    }
+
+    @Sql("classpath:groups_students_data.sql")
+    @Test
+    void findAll_shouldReturnExpectedRowsNumber_whenGroupsExist() {
+        int expectedSize = 5;
+        int actualSize = groupDao.findAll().size();
+
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Sql("classpath:groups_students_data.sql")
+    @Test
+    void findAllWithStudentCountLessThanEqual_shouldReturnExpectedRowsNumber_whenGroupsExist() {
+        int expectedSize = 2;
+        int actualSize = groupDao.findAllWithStudentCountLessThanEqual(3).size();
+
+        assertEquals(expectedSize, actualSize);
+    }
+}
---
+++ src/test/resources/groups_students_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO groups (group_name)
+VALUES ('group1'),
+       ('group2'),
+       ('group3'),
+       ('group4'),
+       ('group5');
+
+INSERT INTO students (group_id, first_name, last_name)
+VALUES (1, 'first_name1', 'last_name1'),
+       (2, 'first_name2', 'last_name2'),
+       (3, 'first_name3', 'last_name3'),
+       (1, 'first_name4', 'last_name4'),
+       (1, 'first_name4', 'last_name4'),
+       (1, 'first_name5', 'last_name5'),
+       (2, 'first_name6', 'last_name6'),
+       (3, 'first_name7', 'last_name7');
