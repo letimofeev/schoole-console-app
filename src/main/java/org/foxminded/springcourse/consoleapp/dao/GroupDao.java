9216cdebbaac6bdf7d97c04c54ef5bcc515f3bc2
leonid <kotovpetia@gmail.com>
2022-11-05 00:53:48 +0300

    GroupDao CRUD added

--- src/main/java/org/foxminded/springcourse/consoleapp/dao/GroupDao.java
+++ src/main/java/org/foxminded/springcourse/consoleapp/dao/GroupDao.java
@@ -3,10 +3,19 @@ package org.foxminded.springcourse.consoleapp.dao;
 import org.foxminded.springcourse.consoleapp.model.Group;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GroupDao {
+
+    void save(Group group);
 
     List<Group> findAll();
 
     List<Group> findAllWithStudentCountLessThanEqual(int studentCount);
+
+    Optional<Group> findById(int id);
+
+    void update(Group group);
+
+    void deleteById(int id);
 }
--- src/main/java/org/foxminded/springcourse/consoleapp/dao/GroupDaoJdbc.java
+++ src/main/java/org/foxminded/springcourse/consoleapp/dao/GroupDaoJdbc.java
@@ -1,11 +1,15 @@
 package org.foxminded.springcourse.consoleapp.dao;
 
 import org.foxminded.springcourse.consoleapp.model.Group;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Repository
 public class GroupDaoJdbc implements GroupDao {
@@ -16,12 +20,27 @@ public class GroupDaoJdbc implements GroupDao {
             "JOIN students s on groups.group_id = s.group_id\n" +
             "GROUP BY groups.group_id, group_name\n" +
             "HAVING count(*) <= ?";
+    public static final String FIND_BY_ID = "SELECT * FROM groups WHERE group_id = ?";
+    public static final String UPDATE_BY_ID = "UPDATE groups SET group_name = ? WHERE group_id = ?";
+    public static final String DELETE_BY_ID = "DELETE FROM groups WHERE group_id = ?";
 
     private final JdbcTemplate jdbcTemplate;
     private final RowMapper<Group> groupRowMapper = new GroupRowMapper();
+    private final SimpleJdbcInsert simpleJdbcInsert;
 
     public GroupDaoJdbc(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("groups")
+                .usingGeneratedKeyColumns("group_id");
+    }
+
+    @Override
+    public void save(Group group) {
+        Map<String, Object> parameters = Map.of("group_name", group.getGroupName());
+
+        Number id = simpleJdbcInsert.executeAndReturnKey(parameters);
+        group.setGroupId(id.intValue());
     }
 
     @Override
@@ -32,5 +51,27 @@ public class GroupDaoJdbc implements GroupDao {
     @Override
     public List<Group> findAllWithStudentCountLessThanEqual(int studentCount) {
         return jdbcTemplate.query(FIND_ALL_WITH_LTE_STUDENTS, groupRowMapper, studentCount);
+    }
+
+    @Override
+    public Optional<Group> findById(int id) {
+        try {
+            Group group = jdbcTemplate.queryForObject(FIND_BY_ID, groupRowMapper, id);
+            return Optional.ofNullable(group);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void update(Group group) {
+        int id = group.getGroupId();
+        String groupName = group.getGroupName();
+        jdbcTemplate.update(UPDATE_BY_ID, groupName, id);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        jdbcTemplate.update(DELETE_BY_ID, id);
     }
 }
