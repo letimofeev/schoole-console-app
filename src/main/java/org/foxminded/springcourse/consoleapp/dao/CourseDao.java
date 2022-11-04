6ff66d686d119c20dbf414d224af7f9d6aa45f25
leonid <kotovpetia@gmail.com>
2022-11-05 00:38:38 +0300

    CourseDao CRUD operations added

--- src/main/java/org/foxminded/springcourse/consoleapp/dao/CourseDao.java
+++ src/main/java/org/foxminded/springcourse/consoleapp/dao/CourseDao.java
@@ -3,9 +3,17 @@ package org.foxminded.springcourse.consoleapp.dao;
 import org.foxminded.springcourse.consoleapp.model.Course;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CourseDao {
 
+     void save(Course course);
+
      List<Course> findAll();
 
+     Optional<Course> findById(int id);
+
+     void update(Course course);
+
+     void deleteById(int id);
 }
